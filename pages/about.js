e2a3c10c444116b4ad39cf7c626ebfc9b3f0d122
net <nati2mulugeta@gmail.com>
2021-12-21 00:40:39 -0800

    initial next app

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+
+export default function AboutPage() {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>This is an app to find the latest events taking place</p>
+            <p>V.0.0</p>
+        </div>
+    )
+}
